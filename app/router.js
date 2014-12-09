58dec3c05fbc8180078d1c8daca3583b9d9d5e55
Michael Zhang <failed.down@gmail.com>
2014-12-09 01:10:59 -0600

    completed router

--- app/router.js
+++ app/router.js
@@ -1,3 +1,25 @@
+var pages = [
+	"index"
+];
+
+var auth_pages = [
+];
+
+for(var i=0; i<auth_pages.length; i++) {
+	pages.push(auth_pages[i]);
+}
+
 module.exports = function(app) {
-	
+	for(var i=0; i<pages.length; i++) {
+		(function(i) {
+			app.get("/" + pages[i], function(req, res) {
+				if (auth_pages.indexOf(pages[i]) > -1) {
+					// TODO Redirect to login page.
+					return;
+				} else {
+					res.sendfile("pages" + req.url + ".html", { root: __dirname });
+				}
+			});
+		})(i);
+	}
 };
